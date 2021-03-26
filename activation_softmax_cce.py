95b01912d2b1a0907978ad33f97d421e17a5f1d8
arif <arif@bitmascot.com>
2021-03-26 12:50:06 +1100

    Base

---
+++ activation_softmax_cce.py
@@ -0,0 +1,20 @@
+from activation_softmax import ActivationSoftmax
+from loss_cce import LossCategoricalCrossentropy
+import numpy as np
+
+
+class ActivationSoftmaxLossCategoricalCrossEntropy:
+    def __init__ ( self ):
+        self.activation = ActivationSoftmax()
+        self.loss = LossCategoricalCrossentropy()
+    def forward ( self , inputs , y_true ):
+        self.activation.forward(inputs)
+        self.output = self.activation.output
+        return self.loss.calculate(self.output, y_true)
+    def backward ( self , dvalues , y_true ):
+        samples = len (dvalues)
+        if len (y_true.shape) == 2 :
+            y_true = np.argmax(y_true, axis = 1 )
+        self.dinputs = dvalues.copy()
+        self.dinputs[ range (samples), y_true] -= 1
+        self.dinputs = self.dinputs / samples
